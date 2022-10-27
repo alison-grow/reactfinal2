784bc7dfe2c32124277909682de523032e72f706
alison-grow <agrowz98@gmail.com>
2022-10-26 22:08:56 -0700

    edited the image slider

--- src/Components/Image Slider/SliderData.js
+++ src/Components/Image Slider/SliderData.js
@@ -1,14 +1,14 @@
 export const SliderData = [
 	{
 		image: "https://images.squarespace-cdn.com/content/v1/5ec88c9b1e20fd1552e11763/1626147264642-TUXL5GHX9XR99YLNHIK6/titlephoto+%281%29.jpg",
-		text: "Your favorite Y2K fashion trends all in one place",
+		text: "2000s fashion trends you will love",
 	},
 	{
 		image: "https://img.buzzfeed.com/buzzfeed-static/static/2021-05/26/14/campaign_images/adca38ac9083/the-terrifying-resurgence-of-y2k-fashion-2-9482-1622040679-36_dblbig.jpg?resize=1200:*",
-		text: "20% off sale on all fall styles!",
+		text: "20% off sale on all seasonal styles!",
 	},
 	{
 		image: "https://miro.medium.com/max/700/1*cKOPg9pp0E8JMCyL0d7bKg.jpeg",
-		text: "Shop the sale now!!",
+		text: "Shop the sale now!",
 	},
 ];
